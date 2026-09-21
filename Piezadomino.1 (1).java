4553721a8443af7bd9cbf7760c43e74b4081d1e1
Brandon1552 <152233740+Brandon1552@users.noreply.github.com>
2026-09-20 18:06:42 -0600

    Refactor elegirModo to use Ficha class
    Refactor 'elegirModo' method to use 'Ficha' class for pieces.

--- Piezadomino.1 (1).java
+++ Piezadomino.1 (1).java
@@ -1,93 +1,114 @@
 import java.util.Scanner;
 
 public class Piezadomino {
-    int ladoA;
-    int ladoB;
-    public int elegirModo(int numero){
-    if(numero == 6){
-        int[] a = new int[35];
-        int totalPiezas = 0;
-        
-        for (int i = 0; i < 7; i++) {
-            for (int j = i; j < 7; j++) {
-                a[totalPiezas] = (i * 10) + j; 
-                totalPiezas++;
-            }
-        }
-        
-        
-        for(int i = 0; i < 7; i++){
-            a[totalPiezas] = (99 * 10) + i; 
-            
-            int ladoA = a[totalPiezas] / 10;
-            int ladoB = a[totalPiezas] % 10;
-            totalPiezas++;
-        }
-        
-        System.out.println("\n--- Fichas Aleatorias ---");
-        for(int k = 0; k < 7; k++){
-            int random = (int) (Math.random() * totalPiezas);
-            int ladoA = a[random] / 10;
-            int ladoB = a[random] % 10;
-            System.out.print("[" + ladoA + "|" + ladoB + "]");
-        }
-        return totalPiezas;
-    }
-    else if(numero == 7){
-        int[] a = new int[44];
-        int totalPiezas = 0;
-        
-        for (int i = 0; i < 8; i++) {
-            for (int j = i; j < 8; j++) {
-                a[totalPiezas] = (i * 10) + j;
-                totalPiezas++;
-            }
-        }
-        
-        for(int i = 0; i < 8; i++){
-            a[totalPiezas] = (99 * 10) + i;
-            int ladoA = a[totalPiezas] / 10;
-            int ladoB = a[totalPiezas] % 10;
 
-            totalPiezas++;
+    public static class Ficha {
+        int ladoA;
+        int ladoB;
+
+        public Ficha(int ladoA, int ladoB) {
+            this.ladoA = ladoA;
+            this.ladoB = ladoB;
         }
-        
-        System.out.println("\n--- Fichas Aleatorias ---");
-        for(int k = 0; k < 7; k++){
-            int random = (int) (Math.random() * totalPiezas);
-            int ladoA = a[random] / 10;
-            int ladoB = a[random] % 10;
-            System.out.print("[" + ladoA + "|" + ladoB + "]");
-        }
-        return totalPiezas;
     }
-    else if(numero == 8){
-        int[] a = new int[54];
-        int totalPiezas = 0;
-        
-        for (int i = 0; i < 9; i++) {
-            for (int j = i; j < 9; j++) {
-                a[totalPiezas] = (i * 10) + j;
+
+    public int elegirModo(int numero) {
+        if (numero == 6) {
+            Ficha[] a = new Ficha[35];
+            int totalPiezas = 0;
+            
+            for (int i = 0; i < 7; i++) {
+                for (int j = i; j < 7; j++) {
+                    a[totalPiezas] = new Ficha(i, j); 
+                    totalPiezas++;
+                }
+            }
+            
+            for (int i = 0; i < 7; i++) {
+                a[totalPiezas] = new Ficha(99, i); 
                 totalPiezas++;
             }
+            
+            System.out.println("\n--- Fichas Aleatorias ---");
+            boolean[] usados = new boolean[totalPiezas];
+            for (int k = 0; k < 7; k++) {
+                int random;
+                do {
+                    random = (int) (Math.random() * totalPiezas);
+                } while (usados[random]);
+                
+                usados[random] = true;
+                
+                int ladoA = a[random].ladoA;
+                int ladoB = a[random].ladoB;
+                System.out.print("[" + (ladoA == 99 ? "C" : ladoA) + "|" + ladoB + "]");
+            }
+            return totalPiezas;
         }
-        
-        for(int i = 0; i < 9; i++){
-            a[totalPiezas] = (99 * 10) + i;
-            int ladoA = a[totalPiezas] / 10;
-            int ladoB = a[totalPiezas] % 10;
-            totalPiezas++;
+        else if (numero == 7) {
+            Ficha[] a = new Ficha[44];
+            int totalPiezas = 0;
+            
+            for (int i = 0; i < 8; i++) {
+                for (int j = i; j < 8; j++) {
+                    a[totalPiezas] = new Ficha(i, j);
+                    totalPiezas++;
+                }
+            }
+            
+            for (int i = 0; i < 8; i++) {
+                a[totalPiezas] = new Ficha(99, i);
+                totalPiezas++;
+            }
+            
+            System.out.println("\n--- Fichas Aleatorias ---");
+            boolean[] usados = new boolean[totalPiezas];
+            for (int k = 0; k < 7; k++) {
+                int random;
+                do {
+                    random = (int) (Math.random() * totalPiezas);
+                } while (usados[random]);
+                
+                usados[random] = true;
+                
+                int ladoA = a[random].ladoA;
+                int ladoB = a[random].ladoB;
+                System.out.print("[" + (ladoA == 99 ? "C" : ladoA) + "|" + ladoB + "]");
+            }
+            return totalPiezas;
         }
-        
-        System.out.println("\n--- Fichas Aleatorias ---");
-        for(int k = 0; k < 7; k++){
-            int random = (int) (Math.random() * totalPiezas);
-            int ladoA = a[random] / 10;
-            int ladoB = a[random] % 10;
-            System.out.print("[" + ladoA + "|" + ladoB + "]");
+        else if (numero == 8) {
+            Ficha[] a = new Ficha[54];
+            int totalPiezas = 0;
+            
+            for (int i = 0; i < 9; i++) {
+                for (int j = i; j < 9; j++) {
+                    a[totalPiezas] = new Ficha(i, j);
+                    totalPiezas++;
+                }
+            }
+            
+            for (int i = 0; i < 9; i++) {
+                a[totalPiezas] = new Ficha(99, i);
+                totalPiezas++;
+            }
+            
+            System.out.println("\n--- Fichas Aleatorias ---");
+            boolean[] usados = new boolean[totalPiezas];
+            for (int k = 0; k < 7; k++) {
+                int random;
+                do {
+                    random = (int) (Math.random() * totalPiezas);
+                } while (usados[random]);
+                
+                usados[random] = true;
+                
+                int ladoA = a[random].ladoA;
+                int ladoB = a[random].ladoB;
+                System.out.print("[" + (ladoA == 99 ? "C" : ladoA) + "|" + ladoB + "]");
+            }
+            return totalPiezas;
         }
-        return totalPiezas;
+        return 0;
     }
-    return 0;
-}
 }
