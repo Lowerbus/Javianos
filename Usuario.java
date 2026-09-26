dc5c3b3e26bdce0a3213c43819e4a678f5edcfa4
Brandon1552 <152233740+Brandon1552@users.noreply.github.com>
2026-09-26 15:45:55 -0600

    Create Usuario class with name property

---
+++ Usuario.java
@@ -0,0 +1,16 @@
+public class Usuario {
+    private String nombre;
+    public Usuario() {
+    }
+
+    public Usuario(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void setnombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+}
